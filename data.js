fc5733531d4b113c6118d252123f52c680896718
RadaRish <rada.rish@ya.ru>
2023-06-15 17:25:47 +0300

    загружен панорамный тур

---
+++ data.js
@@ -0,0 +1,353 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.0009519977738143837,
+        "pitch": -0.011669360057901201,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8673821959907553,
+          "pitch": 0.11294874764413976,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -0.8565880828110366,
+          "pitch": 0.09553912900088335,
+          "rotation": 0,
+          "target": "3-4"
+        },
+        {
+          "yaw": -2.3928701434339956,
+          "pitch": 0.06371466162617168,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1369041521600556,
+          "pitch": 0.8587421639986808,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">свежая выпечка</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">доставляем из домашней пекарни</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.7755898486640547,
+        "pitch": 0.2269218396237136,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.481748100369286,
+          "pitch": 0.14576460774101996,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -1.7418275565870864,
+          "pitch": 0.07505773368958124,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.220907081965656,
+          "pitch": 0.9339357511928821,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">свежая выпечка к завтраку</font></font></font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">из Домашней пекарни</font></font></font></font>"
+        },
+        {
+          "yaw": -0.2159257596664368,
+          "pitch": 0.6574548239773108,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">напитки</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">напитки, фрукты, продукты и не только из магазина Домашний 24</font></font><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.134928669173078,
+        "pitch": -0.09594807158716279,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8914875265507964,
+          "pitch": 0.13506392014600976,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -2.5372807135093325,
+          "pitch": 0.12869542627946373,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.41887902047864145,
+        "pitch": 0.020745528991824358,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5499228970295285,
+          "pitch": 0.1294571510907332,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -1.3843175276135131,
+          "pitch": 0.04877839839188525,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1354264640660157,
+        "pitch": -0.10487988559741979,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3777424858318277,
+          "pitch": 0.12665024798320346,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 2.9093967942663213,
+          "pitch": 0.17466975297867648,
+          "rotation": 0,
+          "target": "5-6"
+        },
+        {
+          "yaw": -2.7683243522243615,
+          "pitch": 0.16570450961611272,
+          "rotation": 0,
+          "target": "6-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16872153142216106,
+          "pitch": 0.8486999481249153,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">большой выбор напитков</font></font></font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">в нашем магазине Домашний 24 есть напитки с доставкой</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.08567979964335493,
+        "pitch": -0.02333872011579885,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19282368187343302,
+          "pitch": 0.21159650997486779,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9578494232150678,
+          "pitch": 0.5792425784759772,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">свежая выпечка</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">у нас можно заказать свежую выпечку из нашей Домашней пекарни</font></font></font></font>"
+        }
+      ]
+    },
+    {
+      "id": "6-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.13042369501266293,
+        "pitch": 0.023338720115795297,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.014257664666629921,
+          "pitch": 0.23161976082182534,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
